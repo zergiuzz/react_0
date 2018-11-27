1a74366f9becabf383236b3194c8bcef0af087a0
zergiuzz <sergzergof@gmail.com>
2018-11-27 20:08:13 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <aside>
+          <ul className="currList">
+          </ul>
+        </aside>
       </div>
     );
   }
