d966cfa014aa1803fe143e20b9a5b462ebf9ddbb
zergiuzz <sergzergof@gmail.com>
2018-11-28 23:09:49 +0600

    codding

--- src/App.js
+++ src/App.js
@@ -5,34 +5,44 @@ import currencies from './currencies.js';
 class App extends Component {
 
   state = {
-    selectPairs: []
+    selectedPairs: [],
   }
 
   handleCheckbox = currency => event =>{
     const {checked} = event.target;
 
-    this.setState(({ selectPairs }) => {
-      return {
-        selectedPairs: [BTC_USD]
+    this.setState(({ selectedPairs }) => {
+      let pairs = [...selectedPairs];
+
+      if (checked) {
+        pairs.push(currency);
       }
-    })
+      else{
+        pairs = pairs.filter(pair => pair !== currency);
+      }
+
+      return {
+        selectedPairs: pairs,
+      };
+    });
   };
 
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <aside>
           <ul className="currList">
             {currencies.map(curr => (
-            <li key={curr} className="currItem">
-            <input type="checkbox" id={curr} onChange={this.handleCheckbox} />
-            <label htmlFor={curr}>
-              {curr}
-            </label>
-            </li>
+              <li key={curr} className="currItem">
+                <input type="checkbox" id={curr} onChange={this.handleCheckbox(curr)}/>
+                <label htmlFor={curr}>{curr.toUpperCase()}</label>
+              </li>
           ))}
           </ul>
         </aside>
+        <main>
+
+        </main>
       </div>
     );
   }
